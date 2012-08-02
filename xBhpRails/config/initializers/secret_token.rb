8ad821071839bfbeed664221323acd893c516ccd
Archan Mishra <archanmishra@gmail.com>
2012-08-02 21:27:56 +0530

    Initial commit.
    Default project setup using scaffold.
    Following the tutorial from guides.rubyonrails.org

---
+++ xBhpRails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+XBhpRails::Application.config.secret_token = '69a2a7664e9d44ecea0fa445b927f1e6189050ee97e89feeee1aa79eb795663b329d0b685132728e701f707c84c30442e5cabfffdd83087243517e26431e6801'
